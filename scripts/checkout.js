2ef3c615a5a7a94dc02f93bd6f434f181d73c9b9
salamiNcheese <salamiguilherme@gmail.com>
2025-11-28 15:40:57 -0300

    Aprender Promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,9 +2,43 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 import { renderProductsGrid } from './amazon.js';
+import { loadCart } from '../data/cart.js';
 //import '../data/cart-class.js';
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+  
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
