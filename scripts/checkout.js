c97fcc53439fa2c466996a51443e3c75287b55a3
salamiNcheese <salamiguilherme@gmail.com>
2025-11-28 16:16:24 -0300

    Converter Primise para Async Await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,21 @@ import { renderProductsGrid } from './amazon.js';
 import { loadCart } from '../data/cart.js';
 //import '../data/cart-class.js';
 
+async function loadPage(){
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -18,7 +33,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
-
+*/
 /*
 new Promise((resolve) => {
   loadProducts(() => {
